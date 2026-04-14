53fa5358f207c49320482ba9d101486058b2d6ca
berji2025 <zerji2025@gmail.com>
2026-04-14 15:20:58 +0800

    Add files via upload

---
+++ liff-init.js
@@ -0,0 +1,15 @@
+window.LIFF_ID = "2009794956-AN7ubbFe";
+
+liff.init({ liffId: window.LIFF_ID })
+  .then(() => {
+    console.log("LIFF 初始化成功");
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    } else {
+      window.dispatchEvent(new CustomEvent('liff-ready'));
+    }
+  })
+  .catch(err => {
+    console.error("LIFF 初始化失敗", err);
+    alert("LINE 初始化失敗，請確認網路連線後重新整理");
+  });
